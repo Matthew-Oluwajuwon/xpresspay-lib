b8e23010a1d262f4e807864676980f26cfbaf465
Matarazy <macbook@Matarazy.local>
2024-06-20 12:39:51 +0100

    project init

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+declare namespace XpressPayPop {
+    type ENV_MODE = 'DEBUG' | 'TEST' | 'LIVE';
+    type DISPLAY_MODE = 'POPUP' | 'PAGE';
+  
+    interface SetupOptions {
+      public_key: string;
+      email: string;
+      amount: string;
+      transactionId: string;
+      currency: string;
+      callbackUrl?: string;
+      metadata?: any;
+      productId?: string;
+      productDescription?: string;
+      applyConviniencyCharge?: boolean;
+      mode?: string;
+      bodyColor?: string;
+      buttonColor?: string;
+      footerText?: string;
+      footerLink?: string;
+      footerLogo?: string;
+      isRecurring?: boolean;
+      authorizationUrl?: string;
+      ENV_MODE?: ENV_MODE;
+      DISPLAY_MODE?: DISPLAY_MODE;
+      onSuccess: (data: any) => void;
+      onError: (error: any) => void;
+    }
+  
+    function setup(options: SetupOptions): void;
+  }
+  
+  interface PayWithXpressPayConfig extends Omit<XpressPayPop.SetupOptions, 'onSuccess' | 'onError'> {
+    onSuccess?: (data: any) => void;
+    onError?: (error: any) => void;
+  }
+  
+  declare function payWithXpressPay(config: PayWithXpressPayConfig): void;
+  
+  export { payWithXpressPay, PayWithXpressPayConfig };
+  
---
+++ index.js
@@ -0,0 +1,63 @@
+// Function to load the XpressPay script
+function loadXpressPayScript() {
+    return new Promise((resolve, reject) => {
+      if (document.getElementById('xpresspay-script')) {
+        resolve();
+        return;
+      }
+  
+      const script = document.createElement("script");
+      script.setAttribute("src", "https://test.xpresspayments.com:8032/inlineXpressPayPop.js");
+      script.setAttribute("id", "xpresspay-script");
+      script.onload = resolve;
+      script.onerror = reject;
+      document.head.appendChild(script);
+    });
+  }
+
+function payWithXpressPay(config) {
+  loadXpressPayScript()
+    .then(() => {
+      XpressPayPop.setup({
+        public_key: config.public_key,
+        email: config.email,
+        amount: config.amount,
+        transactionId:
+          config.transactionId ||
+          "" + Math.floor(Math.random() * 1000000000 + 1),
+        currency: config.currency || "NGN",
+        callbackUrl: config.callbackUrl,
+        metadata: config.metadata,
+        productId: config.productId,
+        productDescription: config.productDescription,
+        applyConviniencyCharge: config.applyConviniencyCharge,
+        mode: config.mode,
+        bodyColor: config.bodyColor,
+        buttonColor: config.buttonColor,
+        footerText: config.footerText,
+        footerLink: config.footerLink,
+        footerLogo: config.footerLogo,
+        isRecurring: config.isRecurring,
+        authorizationUrl: config.authorizationUrl,
+        ENV_MODE: config.ENV_MODE || "DEBUG",
+        DISPLAY_MODE: config.DISPLAY_MODE || "POPUP",
+        onSuccess:
+          config.onSuccess ||
+          ((response) => {
+            console.log("Payment successful:", response);
+          }),
+        onError:
+          config.onError ||
+          ((error) => {
+            console.error("Payment error:", error);
+          }),
+      });
+    })
+    .catch((error) => {
+      console.error("Failed to load XpressPay script:", error);
+    });
+}
+
+module.exports = {
+  payWithXpressPay,
+};
